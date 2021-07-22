dc7e796735a261bbaad45d9ba5c8197ef0c419f9
fulfiljs <87823301+fulfiljs@users.noreply.github.com>
2021-07-22 21:26:46 +0500

    Test

---
+++ darslar_juft/js/app.js
@@ -0,0 +1,82 @@
+/*
+
+var
+
+let / const
+
+// Camel Case camelCase
+// Snake Casse snake_case
+// PascalCase PascalCase
+
+*/
+
+// DataTypes
+// String
+let string = "Bu matn" // "", '', ``;
+// console.log(typeof string);
+// Number
+let number = "41";
+// console.log(typeof number)
+// float integer 3.14151617
+// Boolean
+let rostmi = true; // false true;
+// console.log(typeof rostmi);
+// null;
+
+let car = null;
+// console.log(typeof car)
+
+// undefined
+let answer;
+// console.log(typeof answer)
+
+// Reference
+// Array - Massiv;
+
+let mevalar = [1, "String", true, null, undefined]; // []
+// console.log(typeof mevalar)
+
+// Object;
+// dictionary;
+let mashina = {
+    name: "Audi",
+    yili: 2018
+} // {}
+
+let tv = {
+    kinolar: {
+        2010: ["dasdas"], 
+        2011: [{}]
+    }
+}
+
+// console.log(mashina);
+
+// table, log, error, warn
+// console.table(mashina)
+
+// Operatorlar
+
+// + - *  / % 
+let a = 1;
+let b = 2;
+
+let x = a / b;
+
+
+// %;
+
+// let bolinuvchi = 10;
+// let blouvchi = 3;
+
+// let qoldiq = 10%3
+
+// console.log(qoldiq)
+
+// let y = 4;
+// console.log(y)
+
+// y=1
+
+// console.log(y)
+
